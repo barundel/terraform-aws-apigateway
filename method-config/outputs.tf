1a54169779f4625d11ad664c70172b390f7471b9
Ben Arundel <ben.arundel@cdl.co.uk>
2020-02-03 10:35:37 +0000

    fix: updaitng the method-configs outputs

--- method-config/outputs.tf
+++ method-config/outputs.tf
@@ -1,7 +1,14 @@
 output "resource_id" {
   value = concat(aws_api_gateway_resource.resource.*.id, [""])[0]
+  description = "The resource's identifier"
+}
+
+output "resource_path_part" {
+  value = concat(aws_api_gateway_resource.resource.*.path_part, [""])[0]
+  description = "The complete path for this API resource, including all parent paths"
 }
 
 output "http_method" {
   value = concat(aws_api_gateway_method.method.*.http_method, [""])[0]
+  description = "HTTP Method of the api gateway method"
 }
